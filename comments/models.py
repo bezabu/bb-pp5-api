9712d95a6429943f8952d9146b77624121fdf1b5
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 15:14:10 +0000

    Add comment, like and vote models and migrate

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from reviews.models import Review
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Review
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from reviews.models import Review
 
-# Create your models here.
+LIKE_FLAVOUR = ((0, 'heart'), (1, 'smiley face'), (2, 'thumbs up'), (3, 'laugh'), (4, 'applause'))
+
+class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' makes sure a user can't like the same post twice.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Review, related_name='likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    flavour = models.IntegerField(choices=LIKE_FLAVOUR, default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'review']
+
+    def __str__(self):
+        return f'{self.owner} {self.review}'
