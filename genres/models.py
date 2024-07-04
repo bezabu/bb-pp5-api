8b18d0f2420d37bb4aa6c3f074ed64b34debe7bc
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 15:11:39 +0000

    Add genre & movie models & migrate

--- genres/models.py
+++ genres/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    color = models.SlugField(max_length=7, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}'
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from genres.models import Genre
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+    director = models.CharField(max_length=255, blank=True)
+    genre = models.ManyToManyField(
+        Genre, related_name='movies', default=1
+    )
+    actors = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../clapper_board_vector_oi5zxv'
+    )
+
+    class Meta:
+        ordering = ['-year']
+
+    def __str__(self):
+        return f'{self.title}'
