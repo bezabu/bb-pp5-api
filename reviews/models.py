48a02c9ce7f7332692a2036429c751965037313c
Ben Butler <benbutleranimator@gmail.com>
2024-07-04 15:12:46 +0000

    Add follow and review models and migrate

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
-# Create your models here.
+RATING = ((0, "0 stars"), (1, "1 star"), (2, "2 stars"), (3, "3 stars"), (4, "4 stars"), (5, "5 stars"))
+
+
+
+class Review(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews")
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name="reviews")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True)
+    rating = models.IntegerField(default=3, choices=RATING)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.owner} reviews {self.movie.title}'
